d80fef6e46fe313605e68d190b53bcf102beea29
Priyanka <priyanka2001@gmail.com>
2026-03-12 21:02:00 +0530

    API_URL backtic

--- src/data/AlogoTrade/strategyService.js
+++ src/data/AlogoTrade/strategyService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '${import.meta.env.VITE_BACKEND_URL}/api/strategies';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/strategies`;
 
 // 1. Create New Strategy
 export const createStrategy = async (strategyData) => {
